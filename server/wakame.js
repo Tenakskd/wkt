2421abdf91fb269fa94c0e520e942f4aa50cf6b5
Tenakskd <anzuday7777@gmail.com>
2025-05-31 17:18:59 +0900

    Update wakame.js

--- server/wakame.js
+++ server/wakame.js
@@ -7,7 +7,7 @@ const MAX_TIME = 10000;
 
 async function getapis() {
     try {
-        const response = await axios.get('https://raw.githubusercontent.com/wakame02/wktopu/refs/heads/main/inv.json');
+        const response = await axios.get('https://raw.githubusercontent.com/Tenakskd/ytserver/refs/heads/main/instances.json');
         apis = await response.data;
         console.log('データを取得しました:', apis);
     } catch (error) {
